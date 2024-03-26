8a43bb1b87674be5583cae4ffb25f5d0863680e3
marcgraczyk <marcgraczyk@math.uu.se>
2024-03-26 16:52:25 -0400

    adding a wrapper for the uniswapv2 token

--- contracts/governance/DaoToken.sol
+++ contracts/governance/DaoToken.sol
@@ -1,34 +1,50 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.2;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Wrapper.sol";
 
-contract DaoToken is ERC20Votes {
+contract MyTokenWrapped is ERC20, ERC20Permit, ERC20Votes, ERC20Wrapper {
     constructor(
-        string memory _name,
-        string memory _symbol,
-        uint256 _initSupply
-    ) ERC20(_name, _symbol) ERC20Permit(_name) {
-        _mint(_msgSender(), _initSupply);
-    }
+        IERC20 wrappedToken
+    )
+        ERC20("MyTokenWrapped", "MTK")
+        ERC20Permit("MyTokenWrapped")
+        ERC20Wrapper(wrappedToken)
+    {}
 
     // The functions below are overrides required by Solidity.
+
+    function decimals()
+        public
+        pure
+        override(ERC20, ERC20Wrapper)
+        returns (uint8)
+    {
+        return 18;
+    }
+
     function _afterTokenTransfer(
         address from,
         address to,
         uint256 amount
-    ) internal override(ERC20Votes) {
+    ) internal override(ERC20, ERC20Votes) {
         super._afterTokenTransfer(from, to, amount);
     }
 
-    function _mint(address to, uint256 amount) internal override(ERC20Votes) {
+    function _mint(
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
         super._mint(to, amount);
     }
 
     function _burn(
         address account,
         uint256 amount
-    ) internal override(ERC20Votes) {
+    ) internal override(ERC20, ERC20Votes) {
         super._burn(account, amount);
     }
 }
